6df92ff0728f2cc1a8c0fecbb3a452bbe1e42b95
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-03-05 22:22:10 -0500

    major progress but date default still messed

--- controllers/flight.js
+++ controllers/flight.js
@@ -3,21 +3,26 @@ const moment = require("moment");
 
 //HTTP GET - Load an Add flights form
 exports.flight_create_get = (req, res) => {
+  //   const newFlight = new Flight();
+
   res.render("flight/add");
-  const newFlight = new Flight();
 };
 
 //HTTP Post
 exports.flight_create_post = (req, res) => {
   console.log(req.body);
-  newFlight = Flight(req.body);
-
+  const newFlight = new Flight(req.body);
+  // Obtain the default date
+  // const dt = newFlight.departs;
+  // // Format the date for the value attribute of the input
+  // const departsDate = dt.toISOString().slice(0, 16);
+  // res.render("flight/add", { departsDate });
   //save flight
   //below is an example of promises
-  flight
+  newFlight
     .save() //if this is true, .then line is executed
     .then(() => {
-      res.redirect("/");
+      res.redirect("/flight/index");
     })
     .catch(() => {
       //this catches errors
@@ -25,6 +30,8 @@ exports.flight_create_post = (req, res) => {
       res.send(err);
     });
 };
+//changing value in add.ejs to <%=departsDate%> does not work. value="2022-03-06T08:30"
+
 //HTTP GET - flights index
 exports.flight_index_get = (req, res) => {
   Flight.find()
