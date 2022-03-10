db90eb35bc0f9d9ea9c762dde53144802d6cc54a
[Dylan Kotzer] <[dKotzer@gmail.com]>
2022-03-09 23:22:05 -0500

    before passing out backup

--- models/Flight.js
+++ models/Flight.js
@@ -1,22 +1,20 @@
 const mongoose = require("mongoose");
 
-
 const destinationSchema = mongoose.Schema({
-  airport: {type: String, enum: ['AUS','DFW','DEN',"LAX","SAN"]},
+  airport: { type: String, enum: ["AUS", "DFW", "DEN", "LAX", "SAN"] },
   arrival: Date,
 });
 
 const flightSchema = mongoose.Schema({
-  airline: {type: String, enum: ['American', 'Southwest', 'United']},
+  airline: { type: String, enum: ["American", "Southwest", "United"] },
   airport: { type: String, default: "DEN" },
-  flightNo: {type: Number, min: 10, max: 9999},
-  departs: { type: Date, default: Date.now() + 31540000000},
+  flightNo: { type: Number, min: 10, max: 9999 },
+  departs: { type: Date, default: Date.now() + 31540000000 },
   destinations: [destinationSchema],
 });
 
 const Flight = mongoose.model("Flight", flightSchema);
-const Destination = mongoose.model("Destination", flightSchema);
+// const Destination = mongoose.model("Destination", flightSchema);
 
-module.exports = {Flight};
+module.exports = { Flight };
 // module.exports = Destination;
-
--- models/Ticket.js
+++ models/Ticket.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
 const ticketSchema = mongoose.Schema({
   seat: {type: String, match: /[A-F][1-9]\d?$/},
   price: Number,
-  flight: {Flight},
+  flight:  {type: Schema.Types.ObjectId, ref: 'Flight'},
 });
 
 
